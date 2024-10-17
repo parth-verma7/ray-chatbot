588956e64777943e4f84c641f5f26c8e6d9931d7
parth-verma7 <lcb2021028@iiitl.ac.in>
2024-10-17 20:49:06 +0530

    prompt fixed

--- backend/app/utils/openai_response.py
+++ backend/app/utils/openai_response.py
@@ -6,14 +6,26 @@ client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 def openai_response(query:str, answer) -> str:
 
-    user_query=f'''User's query: {query}. You have to frame the final answer of this 
-    query by extracting answers from this knowledge base - {answer}'''
-
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[{"role": "user", "content": user_query}]
-    )
-    return response.choices[0].message.content
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "system", 
+                    "content": '''You are a helpful AI assistant. Respond concisely and clearly to any user query, either with greetings or information as appropriate. Respond in the same Language as you receive the user's query. Use the Vector DB results when relevant.'''
+                },
+                {
+                    "role": "user", 
+                    "content": f'''
+                        User's query: {query}.
+                        You have to frame the response for this user's query.
+                        If user query is a greeting message or something similar, reply with a conversation greeting message matching the user's tone.
+                        Else if the user is seeking information, use the following Vector DB results to frame the final answer: {answer}.
+                        It is not necessary to use all the results from the Vector DB; use your judgment to frame a concise and precise response.
+                    '''
+                }
+            ]
+        )
+        return response.choices[0].message.content
 
 # print(openai_response("What is science in short?"))
         
